6c4f02c6f36635ce6890cc3636ced4e6db2e2a1c
tr4n <tranquanghuy.hsgs@gmail.com>
2025-08-22 21:04:26 +0700

    smallest infinite set

--- src/local/SmallestInfiniteSet.kt
+++ src/local/SmallestInfiniteSet.kt
@@ -1,13 +1,43 @@
-package home
+package local
+
+import java.util.TreeSet
 
 class SmallestInfiniteSet() {
+    private val treeSet = TreeSet<Int>()
+
+    init {
+        treeSet.addAll(1..1100)
+    }
 
     fun popSmallest(): Int {
-
+        return treeSet.pollFirst() ?: 0
     }
 
     fun addBack(num: Int) {
-
+        treeSet.add(num)
     }
 
+}
+
+fun firstMissingPositive(nums: IntArray): Int {
+    val n = nums.size
+    var i = 0
+    while (i < n) {
+        val num = nums[i]
+        val correctPos = num - 1
+        if (num !in 1..n || i == correctPos) {
+            i++
+            continue
+        }
+
+        val tmp = nums[i]
+        nums[correctPos] = tmp
+        nums[i] = tmp
+        i++
+    }
+
+    for(j in 0 until n) {
+        if(nums[j] !=  j + 1) return j + 1
+    }
+    return n + 1
 }
